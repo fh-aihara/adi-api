63a34705d0a3306deed3f8282c788c988e2c178b
fh-aihara <90545280+fh-aihara@users.noreply.github.com>
2024-05-20 23:38:51 +0900

    クエリされた内容を保存するように変更

--- src/router/adminapi.py
+++ src/router/adminapi.py
@@ -34,7 +34,7 @@ class SQLQuery(BaseModel):
     sql: str
 
 # BigQueryクライアントの初期化
-# client = bigquery.Client()
+client = bigquery.Client()
 
 # router定義
 router = APIRouter()
@@ -82,17 +82,20 @@ def update_queries(query_id: int, query_history: query_histroy, session: Session
     return existing_query
 
 
-# @router.post('/gcp/query')
-# def post_query(query: SQLQuery):
-#     try:
-#         query_job = client.query(query.sql)  # クエリの実行
-#         results = query_job.result()  # クエリ結果の取得
+@router.post('/gcp/query')
+def post_query(query: SQLQuery):
+    try:
+        query_job = client.query(query.sql)  # クエリの実行
+        results = query_job.result()  # クエリ結果の取得
         
-#         # 結果の整形
-#         rows = []
-#         for row in results:
-#             rows.append(dict(row))
+        # 結果の整形
+        rows = []
+        for row in results:
+            rows.append(dict(row))
+        record_count = len(rows)
+        post_query({"SQL": str(query), 
+                    "last_query_records": record_count})
         
-#         return {"results": rows}
-#     except (GoogleAPICallError, NotFound) as e:
-#         raise HTTPException(status_code=400, detail=str(e))
+        return {"results": rows}
+    except (GoogleAPICallError, NotFound) as e:
+        raise HTTPException(status_code=400, detail=str(e))
