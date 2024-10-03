1a036b8f2604f4ccb6bd0bdcd811b1e158ae2f84
fh-aihara <90545280+fh-aihara@users.noreply.github.com>
2024-10-04 00:20:56 +0900

    up

--- src/router/adminapi.py
+++ src/router/adminapi.py
@@ -166,6 +166,11 @@ def format_to_excel(data, property_customer_managed_id, date, building_name):
         ws.cell(row=start_row + index, column=27, value=row['renewal_office_fee_tax'])
         ws.cell(row=start_row + index, column=28, value=row['note'])
 
+    # 不要な行を削除
+    last_row = start_row + len(df) - 1
+    if last_row < 47:
+        ws.delete_rows(last_row + 1, 47 - last_row)
+
     # 出力ファイル名
     output_filename = f"{property_customer_managed_id}_{date}_rentroll.xlsx"
     output_filepath = f"./rentroll/{output_filename}"
